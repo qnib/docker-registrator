cea0fe72ed80ac412f4cf3d3d3b576ee6f1b65e5
Christian Kniep <christian@qnib.org>
2015-12-09 11:40:28 +0100

    add Makefile

---
+++ Makefile
@@ -0,0 +1,7 @@
+TAGS=latest
+NAME=qnib/registrator
+
+include ~/src/github.com/ChristianKniep/QNIBTerminal/docker.mk
+
+all: build
+
--- opt/qnib/registrator/bin/start.sh
+++ opt/qnib/registrator/bin/start.sh
@@ -40,5 +40,28 @@ fi
 if [ "X${REGISTRATOR_FILTER_EXCLUDE}" != "X" ];then
    EXCLUDE=" --exclude=${REGISTRATOR_FILTER_EXCLUDE}"
 fi
+#### Define the target via hostname or environment
+# default
+CONSUL_TARGET=consul.service.consul
+if [ "X${REGISTRATOR_CONSUL_TARGET_CONFD}" == "Xtrue" ];then
+    if [ ! -f /conf.d/hostname ];then
+       echo "WARN >> REGISTRATOR_CONSUL_TARGET_CONFD==true but no /conf.d/hostname found... :("
+       cp /opt/qnib/registrator/etc/registrator_warn.json /etc/consul.d/registrator.json
+       sed -e "s#\"script\": \"echo.*#\"script\": \"echo 'WARN >> REGISTRATOR_CONSUL_TARGET_CONFD==true but no /conf.d/hostname found... :' ; exit 1\",#" /etc/consul.d/registrator.json
+       consul reload
+    else
+       source /conf.d/hostname
+       if [ -z $hostname ];then
+           echo 'WARN >> REGISTRATOR_CONSUL_TARGET_CONFD==true but /conf.d/hostname does not provide a ${hostname} :('
+           cp /opt/qnib/registrator/etc/registrator_warn.json /etc/consul.d/registrator.json
+           sed -e "s#\"script\": \"echo.*#\"script\": \"echo 'WARN >> REGISTRATOR_CONSUL_TARGET_CONFD==true but /conf.d/hostname does not provide a \${hostname} :(' ; exit 1\",#" /etc/consul.d/registrator.json
+           consul reload
+       else
+           CONSUL_TARGET="${hostname}"
+       fi
+    fi
+elif [ "X${REGISTRATOR_CONSUL_TARGET}" != "X" ];then
+   CONSUL_TARGET=${REGISTRATOR_CONSUL_TARGET}
+fi
 
-/usr/local/bin/registrator ${EXCLUDE} ${INCLUDE} ${INTER} ${IP} consul://consul.service.consul:8500
+/usr/local/bin/registrator ${EXCLUDE} ${INCLUDE} ${INTER} ${IP} consul://${CONSUL_TARGET}:8500
